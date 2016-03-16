e96d839393b04fe4b57817e594dd72ae3d6a12ec
Erik Lee Nylen <erikleenylen@gmail.com>
2016-03-16 07:57:24 -0400

    added patodb

--- soundTransformations.py
+++ soundTransformations.py
@@ -9,3 +9,7 @@ import numpy as np
 def dBtoPa(vectin):
   '''convert vector of decibels values to Pascals'''
   return [np.exp(float(_)/float(20))*2/(10**5) for _ in vectin]	
+
+def PatodB(vectin):	
+  '''convert vector (list) of Pascals to decibels'''
+  return [20.*np.log(_*(10**5)/2) for _ in vectin]
